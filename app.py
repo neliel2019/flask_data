9d40ab7a5d01d724d09f3899c74555b6b956c950
qian <ac>
2019-12-06 09:46:16 +0800

    aaa

--- app.py
+++ app.py
@@ -1,13 +1,57 @@
-from flask import Flask
-from tab_user import getpassword
+from flask import Flask, json
+from tab_user import login_cheek
+from tab_user import register_user
 
 from dbconnection import acc
 
+from flask import request
 
-@acc.route('/')
-def hello_world():
-    print(getpassword())
-    return 'Hello World!'
+
+@acc.route('/api/login',methods=['POST','GET'])
+def login_web():
+    ac=request.get_data()
+    json_data=json.loads(ac.decode("utf-8"))
+    username=str(json_data.get("username"))
+    password = str(json_data.get("password"))
+
+    print(username,password)
+    check_code=login_cheek(username,password)
+    if(check_code==1):
+        return {
+            "login_check_code":check_code,
+            "login_message":"登录成功！"
+        }
+    else:
+        return {
+            "login_check_code": check_code,
+            "login_message": "登录失败！"
+        }
+
+@acc.route('/api/register',methods=['POST','GET'])
+def register_web():
+    register_web_request_data = request.get_data()
+    register_web_json= json.loads(register_web_request_data.decode("utf-8"))
+    r_username = str(register_web_json.get("r_username"))
+    r_password = str(register_web_json.get("r_password"))
+    print(r_username,r_password)
+    register_code=register_user(r_username,r_password)
+    if(register_code==1):
+        return {
+            "register_code":register_code,
+            "register_message":"注册成功"
+        }
+    else:
+        return {
+            "register_code": 0,
+            "register_message": "该用户名已被使用"
+        }
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
--- tab_user.py
+++ tab_user.py
@@ -3,9 +3,44 @@ from dbconnection import db
 class User(db.Model):
 
     username=db.Column(db.String(255),primary_key=True)
-    passwrod=db.Column(db.String(255),primary_key=True)
+    passwrod=db.Column(db.String(255))
+    authority=db.Column(db.String(255))
+
+    def __init__(self, username, passwrod,authority):
+        self.username = username
+        self.passwrod = passwrod
+        self.authority=authority
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
-def getpassword():
-    u1=User.query.filter(User.username=="aaaa").all()
-    return u1
+def login_cheek(usernameByWeb,pswByWeb):
+    u1=User.query.filter(User.username==usernameByWeb).first()
+
+    if u1==None:
+        return 0
+
+    elif(usernameByWeb==u1.username and pswByWeb==u1.passwrod):
+
+      return 1
+    else:
+        return 0
+def register_user(r_username,r_password):
+    u2=User.query.filter(User.username==r_username).first()
+    if (u2 == None):
+        new_user = User(r_username, r_password, '1')
+        db.session.add(new_user)
+        db.session.commit()
+        return 1
+    else:
+
+        return 0
+
+
+
+
+
+
+
+
